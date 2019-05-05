9cbf53180b6048ebeb3769a44d04aff184f9cad6
John Jacobsen <46658964+JohnMJacobsen@users.noreply.github.com>
2019-05-04 18:46:03 -0700

    Update ContentGenerator.java

--- SQLFiller/src/SQLFiller/ContentGenerator.java
+++ SQLFiller/src/SQLFiller/ContentGenerator.java
@@ -7,7 +7,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Random;
 import java.util.Scanner;
@@ -22,24 +26,41 @@ public class ContentGenerator {
 	static ArrayList<String> lastNames = new ArrayList<String>();
 	static ArrayList<String> majors = new ArrayList<String>();
 	static ArrayList<SchoolClass> classes = new ArrayList<SchoolClass>();
+	static ArrayList<String> usedClassCodes = new ArrayList<>();
+	static ArrayList<Integer> studentSSNs = new ArrayList<Integer>();
+	static HashMap<Integer, String> nameWithId = new HashMap<>();
 
 	public static void main(String[] args) throws IOException {
+		
+		final int NUM_STUDENTS = 1000;
+		final int NUM_STAFF = 300;
+		final int NUM_CLASSES = 500;
+		final int NUM_STUDENT_CLASS_RELATIONS = 3000;
+		
 
 		fillArrayLists();
 		try {
-			Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("filename.txt"), "utf-8"));	
-			writer.write(createStudentInputs(10));
+			Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("InputLines.txt"), "utf-8"));	
+			writer.write(createStudentInputs(NUM_STUDENTS));
 			((BufferedWriter) writer).newLine();
 			((BufferedWriter) writer).newLine();
-			writer.write(createStaffInputs(10));
+			writer.write(createStaffInputs(NUM_STAFF));
 			((BufferedWriter) writer).newLine();
 			((BufferedWriter) writer).newLine();
-			writer.write(createClassesInputs(10));
-
+			writer.write(createClassesInputs(NUM_CLASSES));
+			((BufferedWriter) writer).newLine();
+			((BufferedWriter) writer).newLine();
+			writer.write(createStaffClassRelationInputs(NUM_CLASSES));
+			((BufferedWriter) writer).newLine();
+			((BufferedWriter) writer).newLine();
+			writer.write(createStudentClassRelationInputs(NUM_STUDENT_CLASS_RELATIONS));
 			writer.close();
 		} catch (IOException e) {
 			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
 		}
+		
 	}
 
 	public static void fillArrayLists() {
@@ -100,7 +121,7 @@ public class ContentGenerator {
 
 		for (int i = 0; i < count; i++) {
 
-			String SSN = setSSN();
+			int SSN = setSSN();
 			char sex = setSex();
 			String firstName = selectNameForSex(sex);
 			String lastName = setLastName();
@@ -119,7 +140,6 @@ public class ContentGenerator {
 
 		String staffInsertLines = "";
 		int staffId;
-		HashMap<Integer, String> nameWithId = new HashMap<>();
 		for (int i = 0; i < count; i++) {
 
 			staffId = i + 1000;
@@ -146,12 +166,141 @@ public class ContentGenerator {
 		return staffInsertLines;
 	}
 	
-	public static String createClassesInputs(int count) {
+	public static String createClassesInputs(int count) throws ParseException {
 		String classesInsertLines = "";
-		
-		
-		
+		for (int i = 0; i < count; i++) {
+			int randIndex = Rand.nextInt(classes.size());
+			SchoolClass chosenClass = classes.get(randIndex);
+			usedClassCodes.add(chosenClass.classCode);
+			classes.remove(randIndex);
+			String days = setDays();
+			String time = selectAccurateTime(days);
+			
+			classesInsertLines += "INSERT INTO Classes VALUES('" + chosenClass.toString() + 
+					days + "', '" + time + "');\n";
+		}
+		System.out.println(classesInsertLines);
 		return classesInsertLines;
+	}
+	
+	public static String createStaffClassRelationInputs(int count) {
+		String staffClassRelationInputs = "";
+		for (int i = 0; i < count; i++) {
+			int staffId = Rand.nextInt(nameWithId.size()) + 1000;
+			String classId = usedClassCodes.get(Rand.nextInt(usedClassCodes.size()));
+			staffClassRelationInputs += "INSERT INTO Staff_Class_Relation VALUES('" + classId + "', " + staffId + ");\n";
+		}
+		return staffClassRelationInputs;
+	}
+	
+	public static String createStudentClassRelationInputs(int count) {
+		String studentClassRelationInputs = "";
+		for (int i = 0; i < count; i++) {
+			String classId = usedClassCodes.get(Rand.nextInt(usedClassCodes.size()));
+			int studentSSN = studentSSNs.get(Rand.nextInt(studentSSNs.size()));
+			studentClassRelationInputs += "INSERT INTO Student_Class_Relation VALUES('" + classId + "', " + studentSSN + ");\n";
+		}
+		return studentClassRelationInputs;
+	}
+
+	private static String selectAccurateTime(String days) throws ParseException {
+		String time = "";
+		if (days.compareTo("M/W/F") == 0) {
+			time = chooseHours(0);
+		}
+		else if (days.compareTo("M/W") == 0 || days.compareTo("T/TH") == 0) {
+			time = chooseHours(1);
+		}
+		else {
+			time = chooseHours(2);
+		}
+		return time;
+	}
+
+	private static String chooseHours(int length) throws ParseException {
+		String startTime = chooseStartTime();
+		SimpleDateFormat df = new SimpleDateFormat("HH:mm");
+		Date d = df.parse(startTime);
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(d);
+		if (length == 0) {
+			cal.add(Calendar.MINUTE, 50);
+		} 
+		else if (length == 1) {
+			cal.add(Calendar.MINUTE, 75);
+		} 
+		else {
+			cal.add(Calendar.MINUTE, 150);
+		}
+		String endTime = df.format(cal.getTime());
+		return startTime + "-" + endTime;
+	}
+	
+	private static String chooseStartTime() {
+		int result = Rand.nextInt(20);
+		switch(result) {
+			case 0:
+				return "8:00";
+			case 1: 
+				return "8:30";
+			case 2:
+				return "9:00";
+			case 3: 
+				return "9:30";
+			case 4:
+				return "10:00";
+			case 5:
+				return "10:30";
+			case 6: 
+				return "11:00";
+			case 7:
+				return "11:30";
+			case 8: 
+				return "12:00";
+			case 9:
+				return "12:30";
+			case 10: 
+				return "13:00";
+			case 11:
+				return "13:30";
+			case 12: 
+				return "14:00";
+			case 13:
+				return "14:30";
+			case 14: 
+				return "15:00";
+			case 15:
+				return "15:30";
+			case 16: 
+				return "16:00";
+			case 17:
+				return "16:30";
+			case 18: 
+				return "17:00";
+			case 19:
+				return "17:30";
+			default: 
+				return "18:00";
+		}
+	}
+	
+	private static String setDays() {
+		int result = Rand.nextInt(5);
+		if (result == 0) {
+			return "M/W/F";
+		}
+		else if (result == 1) {
+			return "M/W";
+		}
+		else if (result == 2) {
+			return "T/TH";
+		}
+		else if (result == 3) {
+			return "T";
+		}
+		else {
+			return "TH";
+		}
 	}
 
 	public static String setMaleName() {
@@ -178,16 +327,23 @@ public class ContentGenerator {
 		}
 	}
 
-	public static String setSSN() {
+	public static int setSSN() {
 		String result = String.valueOf(Rand.nextInt(1000000000));
 		while (result.length() < 9) {
 			result = "0" + result;
 		}
-		return result;
+		int ssn = Integer.parseInt(result);
+		if (!studentSSNs.contains(ssn)) {
+			studentSSNs.add(ssn);
+			return ssn;
+		}
+		else {
+			return setSSN();
+		}
 	}
 
 	public static String setStudentBirthDate() {
-		int year = Rand.nextInt(25) + 1950;
+		int year = Rand.nextInt(25) + 1975;
 		int month = Rand.nextInt(12) + 1;
 		int day = setAccurateDay(month);
 		return (year + "-" + month + "-" + day);
@@ -254,14 +410,19 @@ public class ContentGenerator {
 	}
 	
 	static class SchoolClass {
-		String c_code;
-		String c_name;
-		int c_value;
+		String classCode;
+		String className;
+		int classValue;
 		
-		SchoolClass (String classCode, String className, int classValue) {
-			this.c_code = classCode;
-			this.c_name = className;
-			this.c_value = classValue;
+		SchoolClass (final String cCode, final String cName, final int cValue) {
+			this.classCode = cCode;
+			this.className = cName;
+			this.classValue = cValue;
+		}
+		
+		@Override
+		public String toString() {
+			return (classCode + "', '" + className + "', " + classValue + ", '");
 		}
 	}
 }
